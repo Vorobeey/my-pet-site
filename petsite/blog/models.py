f91e403d4b3ea9e4eac55a73bcfbc3136839ff77
Александр <103884694+Vorobeey@users.noreply.github.com>
2022-09-10 21:45:21 +0300

    Create new project from Django

---
+++ petsite/blog/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=150)
+    content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    is_published = models.BooleanField(default=True)
